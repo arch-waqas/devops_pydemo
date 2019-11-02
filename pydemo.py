04538460393b64d4c9b7dbe6c5a477c9a46719f8
Waqas Azam <waqas@waqas-macbookpro.local>
2019-11-02 20:24:35 +0500

    Some change in root url

--- pydemo.py
+++ pydemo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Alhamdullillah</h1>"
+    return "<h1 style='color:blue'>Alhamdullillah we can now see the pipeline running automatically</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
