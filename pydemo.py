7b5b1207b0f75988eadc6d29960f0a0e8ac2d7ec
Waqas Azam <waqas@waqas-macbookpro.local>
2019-11-02 20:17:34 +0500

    Something was fixed

--- pydemo.py
+++ pydemo.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>DevOps Flask Application Hosted on pydemo.waqasazam.info on Digital Ocean 
-and pushed via Jenkins from Github.com Alhamdullillah!</h1>"
+    return "<h1 style='color:blue'>Alhamdullillah</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
